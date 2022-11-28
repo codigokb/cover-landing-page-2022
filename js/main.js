e4e8f83fd8228abeae9c43eae0e8407cdda3f6e6
codigokb <juankyubey@gmail.com>
2022-11-28 01:45:06 -0200

    Commit JavaScritp

--- js/main.js
+++ js/main.js
@@ -3,24 +3,5 @@ function loadImage(id, targetId) {
     var targetEl = targetId ? document.getElementById(targetId) : el;
     var imageToLoad;
 
-    if (el.dataset.image) {
-        imageToLoad = el.dataset.image;
-    } else if (typeof el.currentSrc === 'undefined') {
-        imageToLoad = el.src;
-    } else {
-        imageToLoad = el.currentSrc;
-    }
-
-    if (imageToLoad) {
-        var img = new Image();
-        img.src = imageToLoad;
-        img.onload = function() {
-            targetEl.classList.add('is-loaded');
-        };
-    }
+    if 
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    loadImage('wallpaper');
-    loadImage('pictureImage', 'picture');
-})
