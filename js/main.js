1e483df557b9a6f56f46e1bed43d8924e4c2a133
codigokb <juankyubey@gmail.com>
2022-12-09 19:18:04 -0200

    Commit final

--- js/main.js
+++ js/main.js
@@ -3,5 +3,24 @@ function loadImage(id, targetId) {
     var targetEl = targetId ? document.getElementById(targetId) : el;
     var imageToLoad;
 
-    if 
-}
+    if (el.dataset.image) {
+        imageToLoad = el.dataset.image;
+    } else if (typeof el.currentSrc === 'undefined') {
+        imageToLoad = el.src;
+    } else {
+        imageToLoad = el.currentSrc;
+    }
+
+    if (imageToLoad) {
+        var img = new Image();
+        img.src = imageToLoad;
+        img.onload = function() {
+            targetEl.classList.add('is-loaded');
+        };
+    }
+} 
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadImage('wallpaper');
+    loadImage('pictureImage', 'picture');
+});
